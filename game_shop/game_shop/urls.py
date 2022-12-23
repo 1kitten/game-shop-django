055effd12be13775482299363b8d7633b4cbdcd0
Maxim Strelchenko <bender404123@gmail.com>
2022-12-23 18:58:37 +0300

    add cart view and html page

--- game_shop/game_shop/urls.py
+++ game_shop/game_shop/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^cart/', include(('cart.urls', 'cart'), namespace='cart')),
     path('users/', include('app_user.urls')),
     path('', include('app_shop.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
